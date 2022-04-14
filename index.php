af36a8c5b33fcfb80bfd7b7e92bbac9d4653d4f5
bulfinsant <100486172+bulfinsant@users.noreply.github.com>
2022-04-14 15:37:18 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Firestorm </title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, maximum-scale=1" />
+<link rel="apple-touch-icon" sizes="57x57" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-114x114">
+<link rel="apple-touch-icon" sizes="120x120" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://firestorm-servers.com/assets/img/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://firestorm-servers.com/assets/img/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://firestorm-servers.com/assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://firestorm-servers.com/assets/img/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://firestorm-servers.com/assets/img/favicon/favicon-16x16.png">
+<style type="text/css" media="screen">
+            body
+            {
+                margin: 0px !important;
+                -webkit-touch-callout: none;
+                -webkit-user-select: none;
+                -khtml-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+            }
+            html
+            {
+                overflow: hidden;
+            }
+        </style>
+</head>
+<body>
+<iframe src="https://api.paymentwall.com/api/ps/?key=89267e86ffa0294792be966fae927034&uid=1&widget=m2_1" width="770px" height="730px" frameborder="0" id="iframe-pw"></iframe>
+</body>
+</html>
